1565226dc08d44554380ac295ba02fc64725ce0d
georgeeset <georgeperfect4u@gmail.com>
2024-01-10 10:42:46 +0100

    app.js: added authentication using email password and jwt
    User.js: user model redesigned to use mysql database indtead of mongodb
    
    temperatureReporter.ino: arduino code used to measure real  temperature of ds18b20 and updated to mqtt every second
    
    listener_client.py: updated with minor changes to print data as they are received

--- test_client/auth_client.py
+++ test_client/auth_client.py
@@ -1,7 +1,7 @@
 import socketio
 import json
 # Server address and port
-server_address = "http://api.esetautomation.tech/socket/v1/"
+server_address = "http://localhost:3000"
 
 # Initialize SocketIO client
 sio = socketio.Client()
@@ -13,51 +13,50 @@ sio = socketio.Client()
 #     mesg = "Hello from client!"
 #     sio.send(mesg)
 
-@sio.event(namespace='/my_assets')
+@sio.event(namespace='/user')
 def connect():
     print("Connected to the server!")
     
     # Subscribe to the 'newclientconnect' event
-    # sio.emit("subscribe", "newclientconnect", "/my_assets")
+    # sio.emit("subscribe", "newclientconnect", '/user')
 
     # Emit the message to the 'newclientconnect' event
-    sio.emit("newclientconnect", 'Im new here', "/my_assets")
+    sio.emit("newclientconnect", 'Im new here', '/user')
 
     #send request to getRecentData channel
-    sio.send("getRecentData", "/my_assets")
+    sio.send("getRecentData", '/user')
 
 
-@sio.event(namespace='/my_assets')
+@sio.event(namespace='/user')
 def connect_error(data):
     print("The connection failed!", data)
 
-@sio.event(namespace='/my_assets')
+@sio.event(namespace='/user')
 def disconnect():
     print("I'm disconnected!")
 
-@sio.on('message', namespace = '/my_assets')
+@sio.on('message', namespace = '/user')
 def message(data):
-    # print(data)
     if (isinstance(data, dict) and 'value' in data.keys()):
         print(f"{data['value']}, {data['date_time']}")
     else:
         print(data)
 
-@sio.on('getRecentData', namespace = '/my_assets')
+@sio.on('getRecentData', namespace = '/user')
 def get_resent_data(data):
     print(data)
 
 
-@sio.on('newclientconnect', namespace='/my_assets')
+@sio.on('newclientconnect', namespace='/user')
 def newclientconnect(data = None):
     print('recieved form channel newclientconnect', data)
 
-@sio.on('getRecentData', namespace='/my_assets')
+@sio.on('getRecentData', namespace='/user')
 def get_recent_data(data = None):
     print('recent data:', data)
 
 
-sio.connect(server_address, namespaces=['/my_assets'], headers={'user':'python'})
+sio.connect(server_address, namespaces=['/user'], headers={'user':'python'})
 
 
 # Send message to server
